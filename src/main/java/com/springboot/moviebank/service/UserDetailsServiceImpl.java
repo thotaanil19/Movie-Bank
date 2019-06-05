3c967c759fbefde07b86606ca88e916e9bfd6493
Anil <thota.anil19@gmail.com>
2019-06-04 21:25:48 -0700

    code clean up

--- src/main/java/com/springboot/moviebank/service/UserDetailsServiceImpl.java
+++ src/main/java/com/springboot/moviebank/service/UserDetailsServiceImpl.java
@@ -9,12 +9,12 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.springboot.moviebank.dao.UserRepository;
 import com.springboot.moviebank.domain.AppUser;
 
-@Component
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
 	@Autowired
